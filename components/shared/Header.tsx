c2be1a2efb9ab3252b5f31d1ae1f664b5ecb17b8
Altono1 <altonobinna11@gmail.com>
2024-09-09 14:57:57 +0100

    footer2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,12 +10,12 @@ const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-28">
+        <Link href="/" className="w-20">
           <Image
             alt=" Event App Logo"
             src="/assets/images/event-logo-1.png"
-            width={100}
-            height={25}
+            width={60}
+            height={15}
           />
         </Link>
         <SignedIn>
